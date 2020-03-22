f15005efd9ce27e16fa3293a24870089180a184b
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-22 15:54:19 -0300

    curso-es6 - criando classes e entendendo herança

--- estudandoes6/main.js
+++ estudandoes6/main.js
@@ -1,10 +1,30 @@
 
-class Teste {
-    metodo(){
-
+class List {
+    constructor(){
+        this.data = []
     }
 
-    outro(){
-
+    add(data){
+        this.data.push(data)
+        console.log(this.data)
     }
 }
+
+class TodoList extends List {
+    constructor(){
+        super()
+        this.usuario = 'Fernando'
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario)
+    }
+}
+
+const minhaLista = new TodoList()
+
+document.getElementById('novotodo').onclick = function(){
+    minhaLista.add('Novo todo')
+}
+
+minhaLista.mostraUsuario()
