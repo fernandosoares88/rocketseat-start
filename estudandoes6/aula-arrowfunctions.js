334425d2763bb84314d7ace2bd9a8c2547ddcfc9
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-22 21:45:23 -0300

    curso-es6 - Aula sobre arrow functions

---
+++ estudandoes6/aula-arrowfunctions.js
@@ -0,0 +1,5 @@
+const arr = [1, 2, 5, 3, 7, 8]
+
+const newArr = arr.map( item => item * 2)
+
+console.log(newArr)
--- estudandoes6/bundle.js
+++ estudandoes6/bundle.js
@@ -1,1 +1,7 @@
 "use strict";
+
+var arr = [1, 2, 5, 3, 7, 8];
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
+console.log(newArr);
