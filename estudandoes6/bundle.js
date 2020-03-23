c8a9451e3538f1cbc3840ff4282bdc0234bc6ec7
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-22 21:53:37 -0300

    curso-es6 - valores padrões em funções

--- estudandoes6/bundle.js
+++ estudandoes6/bundle.js
@@ -1,7 +1,16 @@
 "use strict";
 
-var arr = [1, 2, 5, 3, 7, 8];
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr);
+function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+}
+
+var somaAF = function somaAF() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+};
+
+console.log(soma(1));
+console.log(somaAF());
